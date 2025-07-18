a9f1d90f461bba222ade7de22d8b03b285b49974
Jaeho <68717044+hjaeho95@users.noreply.github.com>
2025-07-18 21:16:59 +0900

    Initial Commit

---
+++ SeSACWeek4_T2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACWeek4_T2
+//
+//  Created by ez on 7/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

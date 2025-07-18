42df2a547aa71221471e79e8ed9e92e15027c917
Jaeho <68717044+hjaeho95@users.noreply.github.com>
2025-07-19 00:44:00 +0900

    Feat: TravelTalk 화면 초기 구성

---
+++ SeSACWeek4_T2/TravelTalk/TravelTalkTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelTalkTableViewCell.swift
+//  SeSACWeek4_T2
+//
+//  Created by ez on 7/19/25.
+//
+
+import UIKit
+
+class TravelTalkTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
